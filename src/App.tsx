391b5982d3ebf5e1249b7fcaa727c0bbceaf6ff8
ChekushkinPM <x-chekushkin-pavel@yandex.ru>
2024-11-20 10:43:27 +0400

    setup current step in cell

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import {useState} from "react";
 
 const SYMBOL_X = 'X' as const;
 const SYMBOL_O = 'O' as const;
@@ -6,8 +7,8 @@ const SYMBOL_O = 'O' as const;
 type Symbol = typeof SYMBOL_X | typeof SYMBOL_O;
 
 function App() {
-    const cells = [SYMBOL_O, null, null, SYMBOL_O, SYMBOL_X, null, null, null, null];
-    const currentStep = SYMBOL_O;
+    const [currentStep, setCurrentStep] = useState<Symbol>(SYMBOL_O);
+    const [cells, setCells] = useState<Array<Symbol | null>>([SYMBOL_O, null, null, SYMBOL_O, SYMBOL_X, null, null, null, null]);
 
     const getSymbolClassName = (symbol: Symbol) => {
         if (symbol === SYMBOL_O) return 'symbol--o';
@@ -17,6 +18,17 @@ function App() {
 
     const renderSymbol = (symbol: Symbol) => <span className={`symbol ${getSymbolClassName(symbol)}`}>{symbol}</span>
 
+    const handleCellClick = (index: number) => {
+        if (cells[index]) {
+            return;
+        }
+
+        const cellsCopy = cells.slice();
+        cellsCopy[index] = currentStep;
+        setCells(cellsCopy);
+        setCurrentStep(currentStep === SYMBOL_O ? SYMBOL_X : SYMBOL_O);
+    }
+
     return (
         <div className="game">
             <div className="game-info">
@@ -24,7 +36,10 @@ function App() {
             </div>
             <div className="game-field">
                 {cells.map((symbol, index) => {
-                    return <button key={index} className="cell">{symbol ? renderSymbol(symbol) : null}</button>
+                    return <button key={index}
+                                   className="cell"
+                                   onClick={() => handleCellClick(index)}
+                    >{symbol ? renderSymbol(symbol) : null}</button>
                 })}
             </div>
         </div>
